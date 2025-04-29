1ef1cab703fb7df16dc5a5a5255aa096669c0100
Wrigley <melkonyanwrigley@gmail.com>
2025-04-29 14:31:36 +0300

    add function for get current directory

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import process from 'node:process';
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-
+import currentPath from './currentPath.js'
+import os from 'node:os'
 process.stdin.resume()
 const rl = readline.createInterface({ input, output });
 
+
+
 console.log("Welcome to the File Manager, "+ process.argv[process.argv.length-1] + '!' )
+const workDirectory = process.cwd()
+const hoeDirectory = os.homedir()
+
+currentPath(hoeDirectory)
 
 rl.on('SIGINT', ()=>{
     console.log("Thank you for using File Manager, " +process.argv[process.argv.length-1]+', goodbye!')
