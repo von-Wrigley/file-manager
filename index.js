2b54461ae43741b0e0e3683ed2d4a881954ac365
Wrigley <melkonyanwrigley@gmail.com>
2025-04-29 14:15:15 +0300

    Add welcome and goodbye

---
+++ index.js
@@ -0,0 +1,19 @@
+import process from 'node:process';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+process.stdin.resume()
+const rl = readline.createInterface({ input, output });
+
+console.log("Welcome to the File Manager, "+ process.argv[process.argv.length-1] + '!' )
+
+rl.on('SIGINT', ()=>{
+    console.log("Thank you for using File Manager, " +process.argv[process.argv.length-1]+', goodbye!')
+    process.exit(0)
+});
+
+rl.on('line', (input) => {
+    if(input === '.exit')
+        console.log("Thank you for using File Manager, " +process.argv[process.argv.length-1]+', goodbye!')
+    process.exit(0)
+  }); 
